6b22fc37e127416246e16b5c4b3b66e1bbbe9a12
JuanEspinoza052000 <juanantonioespinozasevilla@gmail.com>
2025-04-16 23:43:13 -0600

    cambios visualess

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless'; // <-- Importamos el adaptador de Vercel
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // <-- Importante para que funcione el adaptador serverless
+  adapter: vercel({}),
+ // <-- Lo conectamos aquí
   vite: {
     plugins: [tailwindcss()]
   }
